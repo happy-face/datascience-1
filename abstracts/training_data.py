d89b9c28a134ffba699fad97231526f243571c07
Ivana Milovanovic <milovanovichivana@gmail.com>
2019-03-21 20:51:00 -0700

    Moving files around. Fixing minor issues in data preprocessor

--- abstracts/training_data.py
+++ abstracts/training_data.py
@@ -22,7 +22,8 @@ import sklearn.metrics as metrics
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", required=True, nargs = 2, help="Input CSV training and test dataset files")
+    parser.add_argument("-td", "--train-data", required=True, help="Input train data CSV")
+    parser.add_argument("-ted", "--test-data", required=True, help="Input test data CSV")
     parser.add_argument("-o", "--output", required=True, help="Output folder")
     parser.add_argument("--force", action="store_true", help="Overwrites output folder if it already exists")
     parser.add_argument("--binary-relevance-naive-bayes", action="store_true", help="Use binary relevance with naive bayes classifier")
@@ -30,7 +31,6 @@ def parse_args():
     parser.add_argument("-ms", "--max-samples", type=int, help="Maximum number of samples to use for training (0 - use entire dataset).")
 
     return parser.parse_args()
-
 
 def output_results(file, accuracy_score, classification_report, category_to_id, classifier_details):
     file.write("accuracy score: " + str(accuracy_score) + "\n")
@@ -71,6 +71,42 @@ def chi_square_feature_selection(x_train, y_train, x_test, feature_names, featur
     return x_train, x_test, feature_names
 
 
+def normalize_abstracts(df):
+    stopWordList = stopwords.words('english')
+    lemma = WordNetLemmatizer()
+    token = ToktokTokenizer()
+
+    def remove_special_chars(text):
+        str_chars = '`-=~@#$%^&*()_+[!{;”:\’><.,/?”}]'
+        for w in text:
+            if w in str_chars:
+                text = text.replace(w,'')
+        return text
+
+    def lemitize_words(text):
+        words = token.tokenize(text)
+        listLemma =[]
+        for w in words:
+            x = lemma.lemmatize(w,'v')
+            listLemma.append(x)
+        text = " ".join(listLemma)
+        return text
+
+    def stop_words_remove(text):
+        wordList = [x.strip() for x in token.tokenize(text)]
+        removedList = [x for x in wordList if not x in stopWordList]
+        text =" ".join(removedList)
+        return text
+
+    def preprocessing_text(text):
+        text = remove_special_chars(text)
+        text = lemitize_words(text)
+        text = stop_words_remove(text)
+        return text
+
+    df['abstract'] = df['abstract'].map(lambda x: preprocessing_text(x))
+
+
 #
 # training
 #   - preprocessing
@@ -92,11 +128,18 @@ if __name__ == "__main__":
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
-    df_training = pd.read_csv(args.input[0], nrows=args.max_samples)
-    df_training.head()
+    print("Reading train set: %s" % args.train_data)
+    df_train = pd.read_csv(args.train_data, nrows=args.max_samples)
+    df_train.head()
 
-    df_test = pd.read_csv(args.input[1], nrows=args.max_samples)
+    print("Reading test set: %s" % args.test_data)
+    df_test = pd.read_csv(args.test_data, nrows=args.max_samples)
     df_test.head()
+
+    print("Normalizing abstracts")
+    normalize_abstracts(df_train)
+    normalize_abstracts(df_test)
+
 
     #apply one-hot encoding for Binary Relevance
     def one_hot_encoder(tags):
@@ -107,20 +150,20 @@ if __name__ == "__main__":
 
     #Map main categories to integers
     unique_categories = set()
-    for n in df_training.main_categories:
+    for n in df_train.main_categories:
         unique_categories.update(n)
     category_to_id = dict([(j,i) for i, j in enumerate(sorted(unique_categories))])
 
 
-    y_df_training = df_training['main_categories'].apply(one_hot_encoder)
-    y_df_training = pd.DataFrame(y_df_training.values.tolist(), columns=range(0, len(category_to_id)))
+    y_df_train = df_train['main_categories'].apply(one_hot_encoder)
+    y_df_train = pd.DataFrame(y_df_train.values.tolist(), columns=range(0, len(category_to_id)))
 
     y_df_test = df_test['main_categories'].apply(one_hot_encoder)
     y_df_test = pd.DataFrame(y_df_test.values.tolist(), columns=range(0, len(category_to_id)))
 
-    x1_train = df_training['title'].values
-    x2_train = df_training['abstract'].values
-    y_train = y_df_training.values
+    x1_train = df_train['title'].values
+    x2_train = df_train['abstract'].values
+    y_train = y_df_train.values
     x_train = np.vstack((x1_train, x2_train))
     x_train = x_train.T
 
@@ -130,7 +173,7 @@ if __name__ == "__main__":
     x_test = np.vstack((x1_test, x2_test))
     x_test = x_test.T
 
-    print("%d documents in training set" %len(x_train))
+    print("%d documents in train set" %len(x_train))
     print("%d documents in test set" % len(x_test))
 
     print("Extracting features")
