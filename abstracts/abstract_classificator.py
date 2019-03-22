0cdbad62bdfb797697b588e914bcf524175b4f45
Ivana Milovanovic <milovanovichivana@gmail.com>
2019-03-21 22:00:28 -0700

    Consolidating training tools into one

--- abstracts/abstract_classificator.py
+++ abstracts/abstract_classificator.py
@@ -5,17 +5,10 @@ import os
 import operator
 import scipy
 import numpy as np
+import sys
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-
-import nltk
-from nltk.tokenize import ToktokTokenizer
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize import ToktokTokenizer
-
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -27,9 +20,14 @@ from sklearn.model_selection import GridSearchCV
 
 import sklearn.metrics as metrics
 
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import ToktokTokenizer
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", required=True, help="Input CSV dataset file")
+    parser.add_argument("-tr", "--train-data", required=True, help="Input train data CSV")
+    parser.add_argument("-te", "--test-data", required=True, help="Input test data CSV")
     parser.add_argument("-o", "--output", required=True, help="Output folder")
     parser.add_argument("--force", action="store_true", help="Overwrites output folder if it already exists")
     parser.add_argument("--binary-relevance-naive-bayes", action="store_true", help="Use binary relevance with naive bayes classifier")
@@ -37,7 +35,6 @@ def parse_args():
     parser.add_argument("-ms", "--max-samples", type=int, help="Maximum number of samples to use for training (0 - use entire dataset).")
 
     return parser.parse_args()
-
 
 def output_results(file, accuracy_score, classification_report, category_to_id, classifier_details):
     file.write("accuracy score: " + str(accuracy_score) + "\n")
@@ -50,139 +47,17 @@ def output_results(file, accuracy_score, classification_report, category_to_id, 
     category_id_pairs = sorted(category_to_id.items(), key=operator.itemgetter(1))
     for category, id in category_id_pairs:
         file.write("%i\t%s\n" % (id, category))
-        
+
     # write classifier parameters
     file.write("\n")
     file.write("classifier details:\n")
     file.write(classifier_details)
-    
 
 def output_summary(file, best_feature_count, best_feature_ratio, best_accuracy_score, best_classification_report, category_to_id, best_classifier_details):
     file.write("best_feature_count = %d\n" % best_feature_count)
     file.write("best_feature_ratio = %.1f%%\n" % best_feature_ratio)
     file.write("\n")
     output_results(file, best_accuracy_score, best_classification_report, category_to_id, best_classifier_details)
-
-
-#
-# Adds main_categories column
-#
-def generate_main_categories(df):
-    # Extract main categories from tag strings
-    def main_categories(tags):
-        main_tags = [i.split(".") for i in tags if i]
-        categories = [item[0] for item in main_tags]
-        categories = list(set(categories))
-        return categories
-
-    # Handling of physics sub-categories'
-    def physics_tags(tags):
-        physics_categories = ['astro-ph', 'cond-mat', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math-ph', 'nlin',
-                              'nucl-ex', 'nucl-th', 'physics', 'quant-ph']
-        result = ['physics' if item in physics_categories else item for item in tags]
-        return list(set(result))
-
-    df['main_categories'] = df['categories'].apply(main_categories)
-    df['main_categories'] = df['main_categories'].apply(physics_tags)
-
-
-#
-# Writes dataset statistics to file
-#
-
-def output_subcategory_stats(df, output_path, summary_file):
-    # compute stats
-    max_category_count = max(len(x) for x in df['categories'])
-    category_count_to_abstract_count = [0] * max_category_count
-    for categories in df['categories']:
-        category_count_to_abstract_count[len(categories) - 1] += 1
-
-    # generate bar plot
-    category_count_names = [str(x + 1) for x in range(0, max_category_count)]
-    y_pos = np.arange(len(category_count_names))
-    plt.figure()
-    plt.bar(y_pos, category_count_to_abstract_count, align='center', alpha=0.5)
-    plt.xticks(y_pos, category_count_names)
-    plt.ylabel('Abstract Count')
-    plt.xlabel('Subcategory Count')
-    plt.title('Abstract Count vs Subcategory Count')
-    plt.savefig(os.path.join(output_path, 'subcategory_counts.png'))
-
-    # output to summary file
-    summary_file.write("# Subcategory Stats\n")
-    summary_file.write("Subcategory Count\tAbstract Count\n")
-    for i in range(0, len(category_count_to_abstract_count)):
-        summary_file.write("%d\t%d\n" % (i, category_count_to_abstract_count[i]))
-    summary_file.write("\n\n")
-
-
-def output_main_category_stats(df, output_path, summary_file):
-    # compute stats
-    max_category_count = max(len(x) for x in df['main_categories'])
-    category_count_to_abstract_count = [0] * max_category_count
-    for categories in df['main_categories']:
-        category_count_to_abstract_count[len(categories) - 1] += 1
-
-    # generate bar plot
-    category_count_names = [str(x + 1) for x in range(0, max_category_count)]
-    y_pos = np.arange(len(category_count_names))
-    plt.figure()
-    plt.bar(y_pos, category_count_to_abstract_count, align='center', alpha=0.5)
-    plt.xticks(y_pos, category_count_names)
-    plt.ylabel('Abstract Count')
-    plt.xlabel('Main Category Count')
-    plt.title('Abstract Count vs Main Category Count')
-    plt.savefig(os.path.join(output_path, 'maincategory_counts.png'))
-
-    # output to summary file
-    summary_file.write("# Main Category Stats\n")
-    summary_file.write("Main Category Count\tAbstract Count\n")
-    for i in range(0, len(category_count_to_abstract_count)):
-        summary_file.write("%d\t%d\n" % (i, category_count_to_abstract_count[i]))
-    summary_file.write("\n\n")
-
-
-def output_main_category_tuple_stats(df, output_path, summary_file):
-    # compute stats
-    category_tuple_to_count = {}
-    for categories in df['main_categories']:
-        category_tuple = "+".join(sorted(categories))
-        if not category_tuple in category_tuple_to_count:
-            category_tuple_to_count[category_tuple] = 0
-        category_tuple_to_count[category_tuple] += 1
-
-    # generate bar plot
-    sorted_items = sorted(category_tuple_to_count.items(), key=operator.itemgetter(1))
-    category_tuples = []
-    counts = []
-    for category_tuple, count in sorted_items:
-        category_tuples.append(category_tuple)
-        counts.append(count)
-
-    y_pos = np.arange(len(category_tuples))
-    plt.figure(figsize=(15, 20))
-    plt.barh(y_pos, counts, align='center', alpha=0.5)
-    plt.yticks(y_pos, category_tuples)
-    plt.xlabel('Abstract Count')
-    plt.ylabel('Category Combination Count')
-    plt.title('Abstract Count vs Category Combination')
-    plt.savefig(os.path.join(output_path, 'category_combination_counts.png'), dpi=300)
-
-    # output to summary file
-    summary_file.write("# Category Combination Stats\n")
-    summary_file.write("Total Category Combinations: %d\n" % len(category_tuple_to_count))
-    summary_file.write("Category Combination\tAbstract Count\n")
-    for category_tuple, count in reversed(sorted_items):
-        summary_file.write("%s\t%d\n" % (category_tuple, count))
-    summary_file.write("\n\n")
-
-
-def output_dataset_stats(df, output_path):
-    with open(os.path.join(output_path, "dataset_summary.txt"), 'w') as summary_file:
-        output_subcategory_stats(df, output_path, summary_file)
-        output_main_category_stats(df, output_path, summary_file)
-        output_main_category_tuple_stats(df, output_path, summary_file)
-
 
 def get_feature_names(feature_prefix, vectorizer):
     feature_names = vectorizer.get_feature_names()
@@ -200,6 +75,41 @@ def chi_square_feature_selection(x_train, y_train, x_test, feature_names, featur
     return x_train, x_test, feature_names
 
 
+def normalize_abstracts(df):
+    stopWordList = stopwords.words('english')
+    lemma = WordNetLemmatizer()
+    token = ToktokTokenizer()
+
+    def remove_special_chars(text):
+        str_chars = '`-=~@#$%^&*()_+[!{;”:\’><.,/?”}]'
+        for w in text:
+            if w in str_chars:
+                text = text.replace(w,'')
+        return text
+
+    def lemitize_words(text):
+        words = token.tokenize(text)
+        listLemma =[]
+        for w in words:
+            x = lemma.lemmatize(w,'v')
+            listLemma.append(x)
+        text = " ".join(listLemma)
+        return text
+
+    def stop_words_remove(text):
+        wordList = [x.strip() for x in token.tokenize(text)]
+        removedList = [x for x in wordList if not x in stopWordList]
+        text =" ".join(removedList)
+        return text
+
+    def preprocessing_text(text):
+        text = remove_special_chars(text)
+        text = lemitize_words(text)
+        text = stop_words_remove(text)
+        return text
+
+    df['abstract'] = df['abstract'].map(lambda x: preprocessing_text(x))
+
 
 #
 # training
@@ -212,7 +122,6 @@ def chi_square_feature_selection(x_train, y_train, x_test, feature_names, featur
 #
 #
 
-
 if __name__ == "__main__":
     args = parse_args()
 
@@ -223,94 +132,58 @@ if __name__ == "__main__":
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
-    df = pd.read_csv(args.input, nrows=args.max_samples)
-    df.head()
+    print("Reading train set: %s" % args.train_data)
+    df_train = pd.read_csv(args.train_data, nrows=args.max_samples)
+    df_train.head()
 
-    # split categories string into list of categories
-    df['categories'] = df['categories'].str.split()
+    print("Reading test set: %s" % args.test_data)
+    df_test = pd.read_csv(args.test_data, nrows=args.max_samples)
+    df_test.head()
 
-    generate_main_categories(df)
-    output_dataset_stats(df, args.output)
+    print("Normalizing abstracts")
+    normalize_abstracts(df_train)
+    normalize_abstracts(df_test)
 
-    #what are main categories?
-    unique_categories = set()
-    for n in df.main_categories:
-        unique_categories.update(n)
-    category_to_id = dict([(j,i) for i, j in enumerate(sorted(unique_categories))])
+    # main categories is list serialized as string, convert it back
+    df_train['main_categories'] = df_train['main_categories'].apply(eval)
+    df_test['main_categories'] = df_test['main_categories'].apply(eval)
 
-    def OneHotEncoder(tags):
+    #apply one-hot encoding for Binary Relevance
+    def one_hot_encoder(tags):
         vec = [0] * len(category_to_id)
         for tag in tags:
             vec[category_to_id[tag]]=1
         return vec
 
-    y_df = df['main_categories'].apply(OneHotEncoder)
-    y_df = pd.DataFrame(y_df.values.tolist(), columns=range(0, len(category_to_id)))
-
-    print("processing abstract text")
-    #ABSTRACT TEXT PROCESSING
-    stopWordList = stopwords.words('english')
-
-    lemma = WordNetLemmatizer()
-    token = ToktokTokenizer()
-
-    def removeSpecialChars(text):
-        str_chars = '`-=~@#$%^&*()_+[!{;”:\’><.,/?”}]'
-        for w in text:
-            if w in str_chars:
-                text = text.replace(w,'')
-        return text
-
-    def lemitizeWords(text):
-        words = token.tokenize(text)
-        listLemma =[]
-        for w in words:
-            x = lemma.lemmatize(w,'v')
-            listLemma.append(x)
-        text = " ".join(listLemma)
-        return text
-
-    def stopWordsRemove(text):
-        wordList = [x.strip() for x in token.tokenize(text)]
-        removedList = [x for x in wordList if not x in stopWordList]
-        text =" ".join(removedList)
-        return text
-
-    def preprocessingText(text):
-        text = removeSpecialChars(text)
-        text = lemitizeWords(text)
-        text = stopWordsRemove(text)
-        return text
-
-    df['abstract'] = df['abstract'].map(lambda x: preprocessingText(x))
-
-    #wordcloud representation
-    import sys
-
-    totalText = ''
-    for x in df.abstract:
-        totalText = totalText + ' ' + x
+    #Map main categories to integers
+    unique_categories = set()
+    for n in df_train.main_categories:
+        print n
+        unique_categories.update(n)
+    print unique_categories
+    category_to_id = dict([(j,i) for i, j in enumerate(sorted(unique_categories))])
 
 
-    wc = WordCloud(background_color='black', max_font_size=50).generate(totalText)
-    plt.figure(figsize=(16, 12))
-    plt.imshow(wc, interpolation="bilinear")    # mapping integer feature names to original token string
-    plt.savefig(os.path.join(args.output, 'word_cloud.png'))
+    y_df_train = df_train['main_categories'].apply(one_hot_encoder)
+    print y_df_train
+    y_df_train = pd.DataFrame(y_df_train.values.tolist(), columns=range(0, len(category_to_id)))
 
+    y_df_test = df_test['main_categories'].apply(one_hot_encoder)
+    y_df_test = pd.DataFrame(y_df_test.values.tolist(), columns=range(0, len(category_to_id)))
 
-    x1 = df['title'].values
-    x2 = df['abstract'].values
-    y = y_df.values
-    x = np.vstack((x1, x2))
-    x = x.T
+    x1_train = df_train['title'].values
+    x2_train = df_train['abstract'].values
+    y_train = y_df_train.values
+    x_train = np.vstack((x1_train, x2_train))
+    x_train = x_train.T
 
-    ##MAIN CATEGORY CLASSIFICATION
-    #Binary relevance
+    x1_test = df_test['title'].values
+    x2_test = df_test['abstract'].values
+    y_test = y_df_test.values
+    x_test = np.vstack((x1_test, x2_test))
+    x_test = x_test.T
 
-    # split a training set and a test set
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
-
-    print("%d documents in training set" %len(x_train))
+    print("%d documents in train set" %len(x_train))
     print("%d documents in test set" % len(x_test))
 
     print("Extracting features")
@@ -363,11 +236,11 @@ if __name__ == "__main__":
                     'classifier__C': [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
                 }
             ]
-            classifier = GridSearchCV(BinaryRelevance(), parameters, scoring='accuracy', n_jobs = 5)
+            classifier = GridSearchCV(BinaryRelevance(), parameters, scoring='accuracy')
             classifier.fit(x_train_sel, y_train)
             classifier_details = ""
             for key, value in classifier.cv_results_.items():
-                classifier_details += "\t".join([key, value]) + "\n"
+                classifier_details += "\t".join([str(key), str(value)]) + "\n"
         else:
             print("ERROR: specify classification model")
             exit()
