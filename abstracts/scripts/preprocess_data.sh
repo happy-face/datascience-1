952aa3000d402adab430ba046ae7e18b78d33026
Ivana Milovanovic <milovanovichivana@gmail.com>
2019-03-23 19:56:58 -0700

    Add new dataset

--- abstracts/scripts/preprocess_data.sh
+++ abstracts/scripts/preprocess_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 preprocess_data.py -i raw_data/test_data_jan_2018.csv -o data/test --force
-python3 preprocess_data.py -i raw_data/train_data_feb_march_2018.csv -o data/train --force
+#python3 preprocess_data.py -i raw_data/test_data_jan_2018.csv -o data/test
+#python3 preprocess_data.py -i raw_data/train_data_feb_march_2018.csv -o data/train
+python3 preprocess_data.py -i raw_data/train_data_jan_june_2018.csv -o data/train_jan_june_2018 --force
