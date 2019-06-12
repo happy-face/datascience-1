b2203294190bef979d782cd984707ef7d6a36ef3
ivanamilovanovic <milovanovichivana@gmail.com>
2019-06-11 22:39:38 -0700

    Code refactoring

--- perfect_shot/feature_extraction.py
+++ perfect_shot/feature_extraction.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import math
 import cv2
+import csv
 
 from imutils import face_utils
 import imutils
@@ -50,18 +51,33 @@ def estimate_general_quality(img):
         sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
         return sigma
 
+    def estimate_brightness(img):
+        histogram = cv2.calcHist([img], [0], None, [256], [0, 256])
+        pixels = sum(histogram)
+        brightness = scale = len(histogram)
+
+        for index in range(0, scale):
+            ratio = histogram[index] / pixels
+            brightness += ratio * (-scale + index)
+
+        return 1 if brightness == 255 else brightness / scale
 
     blur_score = estimate_blur(img)
     print("Blurrines of the image: ", blur_score)
     noise_score = estimate_noise(img)
     print("Noise in the image: ", noise_score)
-    return blur_score, noise_score
+    bright_score = estimate_brightness(img)
+    print("Brightness of the image: ", bright_score)
+    return blur_score, noise_score, bright_score
 
 #must have gray image as input
 def face_detector(img):
-    #using HAAR Cascade Classifier
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
+
+    # initialize dlib's face detector (HOG-based)
+    detector = dlib.get_frontal_face_detector()
+
+    # detect faces in the grayscale image
+    faces = detector(img, 1)
     print('Faces found: ', len(faces))
 
     if len(faces) == 0:
@@ -69,36 +85,41 @@ def face_detector(img):
 
     return faces
 
-def eyes_detector(img):
+#gray image as input!!!
+def face_landmarks_detector(img, faces, im_name, out_path):
 
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
-    eyes = eye_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
-    print('Eyes found: ', len(eyes))
+    # create the facial landmark predictor
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-    if len(eyes) == 0:
-        print('No eyes detected')
+    face_landmarks = []
 
-    return eyes
+    for (i,rect) in enumerate(faces):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(img, rect)
+        shape = face_utils.shape_to_np(shape)
 
-def get_face_roi(img, im_name, faces):
-    face_roi = []
-    if faces.any():
-        for (x, y, w, h) in faces:
-            face_roi.append(img[y:(y + h), x:(x + w)])
-    return face_roi
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-#requires grayscale image
-def calculate_brightness(img):
+        # show the face number
+        cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    histogram = cv2.calcHist([img], [0], None, [256], [0,256])
-    pixels = sum(histogram)
-    brightness = scale = len(histogram)
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-    for index in range(0, scale):
-        ratio = histogram[index] / pixels
-        brightness += ratio * (-scale + index)
+        face_landmarks.append(shape)
 
-    return 1 if brightness == 255 else brightness / scale
+    # show the output image with the face detections
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
+    cv2.imwrite(os.path.join(out_path, im_name + '_face_landmarks.png'),image)
+
+    return face_landmarks
 
 
 def eye_aspect_ratio(eye):
@@ -118,49 +139,21 @@ def eye_aspect_ratio(eye):
     return ear
 
 
-
-#requires grayscale image
-def detect_facial_landmarks(img):
-    # initialize dlib's face detector (HOG-based) and then create
-    # the facial landmark predictor
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-
-    # detect faces in the grayscale image
-    rects = detector(img, 1)
+def closed_eyes_detector(face_shapes):
 
     EYE_AR_THRESH = 0.1
     # grab the indexes of the facial landmarks for the left and
-    # right eye, respectively
+    # right eye
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    closed_eyes_image = []
-    for (i,rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(img, rect)
-        shape = face_utils.shape_to_np(shape)
-
-        # convert dlib's rectangle to a OpenCV-style bounding box
-        # [i.e., (x, y, w, h)], then draw the face bounding box
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # show the face number
-        cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        for (x, y) in shape:
-            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
+    closed_eyes_in_image = []
+    for (i,shape) in enumerate(face_shapes):
+        # extract the left and right eye coordinates
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
+
+        # compute the eye aspect ratio for both eyes
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
 
@@ -179,15 +172,16 @@ def detect_facial_landmarks(img):
             eye_closed = 1
 
         closed_eye.append(eye_closed)
+        closed_eyes_in_image.append(closed_eye)
 
-        closed_eyes_image.append(closed_eye)
+    return closed_eyes_in_image
 
-    # show the output image with the face detections + facial landmarks
-    cv2.imshow("Output", image)
-    cv2.waitKey(0)
 
-    return closed_eyes_image
+def get_face_roi(img, face):
 
+    (x, y, w, h) = face_utils.rect_to_bb(face)
+    face_roi = img[y:(y + h), x:(x + w)]
+    return face_roi
 
 
 if __name__ == "__main__":
@@ -202,69 +196,47 @@ if __name__ == "__main__":
 
     #import image list
     im_files = os.listdir(args.im_path)
+
     df = pd.DataFrame(im_files, columns=['file_name'])
 
 
-    blur_all = []
-    noise_all = []
-    faces_all = []
-    eyes_all = []
-    face_brightness_all = []
-    closed_eyes_all = []
-
+    table = []
     for im_name in im_files:
 
-        image = cv2.imread(os.path.join('images/', im_name))
+        image = cv2.imread(os.path.join(args.im_path, im_name))
 
-        #convert to gray (HAAR requirement)
+        #convert to gray
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
-        blur, noise = estimate_general_quality(gray_img)
-
-        blur_all.append(blur)
-        noise_all.append(noise)
+        blur, noise, brightness = estimate_general_quality(gray_img)
 
         #face region detection
         faces = face_detector(gray_img)
-        faces_all.append(faces)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        number_of_faces = len(faces)
 
-        plt.figure()
-        plt.imshow(convertToRGB(image))
-        plt.savefig(os.path.join(args.output, im_name + '_faces_detected.png'))
+        #EXTRACT FACE FEATURES
 
-        #eyes detection
-        eyes = eyes_detector(gray_img)
-        eyes_all.append(eyes)
-        for (x, y, w, h) in eyes:
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        faces_blur_all = []
+        faces_noise_all = []
+        faces_brightness_all = []
 
-        plt.figure()
-        plt.imshow(convertToRGB(image))
-        plt.savefig(os.path.join(args.output, im_name + '_eyes_detected.png'))
+        for face in faces:
+            # extract face ROI
+            face_roi = get_face_roi(gray_img, face)
+            face_blur, face_noise, face_brightness = estimate_general_quality(face_roi)
 
-        face_roi = get_face_roi(gray_img, im_name, faces)
-        '''#save faces ROI
-        for i, face in enumerate(face_roi):
-            cv2.imwrite(os.path.join(args.output, im_name + '_face' + str(i) + '_eyes_detected.png'), face)'''
+            faces_blur_all.append(face_blur)
+            faces_noise_all.append(face_noise)
+            faces_brightness_all.append(face_brightness)
 
-        face_brightness_image= []
-        for f in face_roi:
-            face_brightness = calculate_brightness(f)
-            face_brightness_image.append(face_brightness)
-        face_brightness_all.append(face_brightness_image)
-        print("Face brightness: ", face_brightness_image)
+        landmarks = face_landmarks_detector(gray_img, faces, im_name, args.output)
 
-        closed_eyes = detect_facial_landmarks(gray_img)
-        closed_eyes_all.append(closed_eyes)
+        closed_eyes = closed_eyes_detector(landmarks)
 
-    df['blur'] = blur_all
-    df['noise'] = noise_all
-    df['faces'] = faces_all
-    df['eyes'] = eyes_all
-    df['face_brightness'] = face_brightness_all
-    df['closed_eyes'] = closed_eyes_all
+        table_entry = [im_name, blur, noise, brightness, faces, number_of_faces, faces_blur_all, faces_noise_all, faces_brightness_all, closed_eyes]
+        table.append(table_entry)
 
-    df.to_csv(os.path.join(args.output, 'results_processed.csv'))
+    df_output = pd.DataFrame(table, columns = ['im_file','blur', 'noise', 'brightness', 'faces', 'number_of_faces', 'faces_blur_all', 'faces_noise_all',
+                   'faces_brightness_all', 'closed_eyes'])
+
+    df_output.to_csv(os.path.join(args.output, 'results_processed.csv'), sep='\t')
